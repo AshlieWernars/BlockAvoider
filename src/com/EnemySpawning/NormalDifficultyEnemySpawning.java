322724dff5a462bb627cf15188987a7d6294ccda
Ashlie <a.wernars@outlook.com>
2024-05-28 08:50:00 +0200

    Added switch case statement for normal difficulty

--- src/com/EnemySpawning/NormalDifficultyEnemySpawning.java
+++ src/com/EnemySpawning/NormalDifficultyEnemySpawning.java
@@ -8,31 +8,41 @@ import com.Main.Game;
 
 public class NormalDifficultyEnemySpawning {
 	
-	public static void spawnNormal(Random r, Game game, Spawner spawner, EntityHandler entityHandler) { //TODO: Switch case
-		if (GameHandler.getLevel() == 1) {
-            entityHandler.addObject(new NormalEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, r));
-        } else if (GameHandler.getLevel() == 4) {
-            entityHandler.addObject(new LineEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, game, r, true));
-        } else if (GameHandler.getLevel() == 5) {
-            entityHandler.addObject(new SmartEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler));
-        } else if (GameHandler.getLevel() == 7) {
-            entityHandler.addObject(new NormalEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, r));
-        } else if (GameHandler.getLevel() == 10) {
-        	spawner.spawnEnemyBoss();
-        } else if (GameHandler.getLevel() == 15) {
-        	spawner.deleteEnemyBoss();
+	public static void spawnNormal(Random r, Game game, Spawner spawner, EntityHandler entityHandler) {
+		switch(GameHandler.getLevel()) {
+		case 1:
+			entityHandler.addObject(new NormalEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, r));
+			break;
+		case 4:
+			entityHandler.addObject(new LineEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, game, r, true));
+			break;
+		case 5:
+			entityHandler.addObject(new SmartEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler));
+			break;
+		case 7:
+            entityHandler.addObject(new NormalEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, r));			
+			break;
+		case 10:
+			spawner.spawnEnemyBoss();			
+			break;
+		case 15:
+			spawner.deleteEnemyBoss();
             entityHandler.addObject(new SmartEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler));
             entityHandler.addObject(new NormalEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, r));
             entityHandler.addObject(new HardEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, r));
             entityHandler.addObject(new LineEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, game, r, true));
             entityHandler.addObject(new LineEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, game, r, false));
-        } else if(GameHandler.getLevel() == 17) {    
-        	entityHandler.addObject(new SmartEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler));
-        } else if(GameHandler.getLevel() == 18) {
-        	spawner.restoreHealth();
-        } else if(GameHandler.getLevel() == 20) {
-        	spawner.spawnMiniGunBoss();
-        } else if(GameHandler.getLevel() == 25) {	
+			break;
+		case 17:
+        	entityHandler.addObject(new SmartEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler));			
+			break;
+		case 18:
+        	spawner.restoreHealth();			
+			break;
+		case 20:
+        	spawner.spawnMiniGunBoss();			
+			break;
+		case 25:
         	spawner.deleteMiniGunBoss();
         	spawner.restoreHealth();
         	entityHandler.addObject(new SmartEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler));
@@ -41,8 +51,11 @@ public class NormalDifficultyEnemySpawning {
             entityHandler.addObject(new HardEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, r));
             entityHandler.addObject(new LineEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, game, r, false));
             entityHandler.addObject(new LineEnemy(r.nextInt(GameHandler.spawnWidth), r.nextInt(GameHandler.spawnHeight), entityHandler, game, r, false));
-        } else if (GameHandler.getLevel() == 30) {
-        	spawner.gameEndWin();
-        }
+			
+			break;
+		case 30:
+        	spawner.gameEndWin();			
+			break;
+		}
 	}
 }
